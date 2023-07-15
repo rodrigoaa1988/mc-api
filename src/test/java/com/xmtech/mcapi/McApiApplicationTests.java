214f9da02e7f080d2c16e0de6dd0cc2ce8141e43
Rodrigo Almeida <rodrigoanjosalmeida@gmail.com>
2023-07-14 23:10:27 -0300

    project created

---
+++ src/test/java/com/xmtech/mcapi/McApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmtech.mcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
