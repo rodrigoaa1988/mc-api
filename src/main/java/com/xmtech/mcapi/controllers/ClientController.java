adb5a6aca71345ad8b9feb648348bd7f0b7d8ee1
Rodrigo Almeida <rodrigoanjosalmeida@gmail.com>
2023-07-21 23:21:35 -0300

    Update and delete clients

--- src/main/java/com/xmtech/mcapi/controllers/ClientController.java
+++ src/main/java/com/xmtech/mcapi/controllers/ClientController.java
@@ -1,20 +1,23 @@
 package com.xmtech.mcapi.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.xmtech.mcapi.dtos.ClientDTO;
 import com.xmtech.mcapi.services.ClientService;
+
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -37,6 +40,21 @@ public class ClientController {
 	@GetMapping("/{client_information}/filter")
 	public ResponseEntity<ClientDTO> findById(@PathVariable("client_information") String clientInformation, @RequestParam("type") String type){
 		return ResponseEntity.ok().body(clientService.findByParam(type, clientInformation));
+	}
+	
+	@PostMapping("")
+	public ResponseEntity<ClientDTO> save(@Valid @RequestBody ClientDTO clientDto){
+		return ResponseEntity.ok().body(clientService.save(clientDto));
+	}
+	
+	@PatchMapping("{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO client){
+		return ResponseEntity.ok().body(clientService.update(id, client));
+	}
+	
+	@DeleteMapping("{id}")
+	public void delete(@PathVariable Long id) {
+		clientService.delete(id);
 	}
 
 }
--- src/main/java/com/xmtech/mcapi/services/ClientService.java
+++ src/main/java/com/xmtech/mcapi/services/ClientService.java
@@ -2,6 +2,7 @@ package com.xmtech.mcapi.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
@@ -10,7 +11,9 @@ import org.springframework.stereotype.Service;
 
 import com.xmtech.mcapi.dtos.ClientDTO;
 import com.xmtech.mcapi.dtos.PhoneDTO;
+import com.xmtech.mcapi.entities.AddressEntity;
 import com.xmtech.mcapi.entities.ClientEntity;
+import com.xmtech.mcapi.entities.PhoneEntity;
 import com.xmtech.mcapi.repositories.AddressRepository;
 import com.xmtech.mcapi.repositories.ClientRepository;
 import com.xmtech.mcapi.repositories.PhoneRepository;
@@ -48,6 +51,74 @@ public class ClientService {
 			clientDto = phoneRepository.findByPhone(clientInformation).getClient().toDto();
 		}
 		return clientDto;
+	}
+	
+	public ClientDTO save(ClientDTO clientDto) {
+		List<PhoneDTO> phoneDtoList = clientDto.getPhoneList();
+		List<PhoneEntity> phoneEntityList = new ArrayList<>();
+		for(PhoneDTO phoneIndex : phoneDtoList) {
+			phoneEntityList.add(phoneIndex.toEntity());
+		}
+		clientDto.setPhoneList(null);
+		AddressEntity addressEntity = clientDto.getAddress().toEntity();
+		clientDto.setAddress(null);
+		ClientEntity clientEntity = clientRepository.save(clientDto.toEntity());
+		for(PhoneEntity phoneIndex: phoneEntityList) {
+			phoneIndex.setClient(clientEntity);
+		}
+		clientEntity.setPhoneList(phoneRepository.saveAll(phoneEntityList));
+		clientEntity.setAddress(addressEntity);
+		addressEntity.setClient(clientEntity);
+		clientEntity = clientRepository.save(clientEntity);
+		return clientEntity.toDto();
+	}
+	
+	public ClientDTO update(final Long id, final ClientDTO client) {
+		ClientEntity clientEntity = new ClientEntity();
+		Optional<ClientEntity> clientOptional = clientRepository.findById(id);
+		if(clientOptional.isPresent()) {
+			clientEntity.setId(id);
+			clientEntity.setDocNumber(client.getDocNumber());
+			clientEntity.setName(client.getName());
+			clientEntity.setBirthDate(client.getBirthDate());
+			
+			for(PhoneDTO phoneIndex : client.getPhoneList()) {
+				Optional<PhoneEntity> phoneOptional = phoneRepository.findById(phoneIndex.getId());
+				if(phoneOptional.isPresent()) {
+					PhoneEntity phoneEntity = phoneIndex.toEntity();
+					phoneEntity.setClient(clientEntity);
+					phoneRepository.save(phoneEntity);
+				}
+			}	
+			Optional<AddressEntity> addressOptional = addressRepository.findById(client.getAddress().getId());
+			if(addressOptional.isPresent()) {
+				AddressEntity addressEntity = client.getAddress().toEntity();
+				addressEntity.setClient(clientEntity);
+				clientEntity.setAddress(addressEntity);
+			}
+		}
+		return clientRepository.save(clientEntity).toDto();
+	}
+	
+	public void delete(final Long id) {
+		ClientEntity clientEntity = new ClientEntity();
+		Optional<ClientEntity> clientOptional = clientRepository.findById(id);
+		if(clientOptional.isPresent()) {
+			clientEntity = clientOptional.get();
+			for(PhoneEntity phoneIndex : clientEntity.getPhoneList()) {
+				Optional<PhoneEntity> phoneOptional = phoneRepository.findById(phoneIndex.getId());
+				if(phoneOptional.isPresent()) {
+					phoneRepository.deleteById(phoneIndex.getId());
+				}
+			}
+			Optional<AddressEntity> addressOptional = addressRepository.findById(clientEntity.getAddress().getId());
+			if(addressOptional.isPresent()) {
+				addressRepository.deleteById(clientEntity.getAddress().getId());
+			}
+			clientRepository.deleteById(id);
+		}
+		
+		
 	}
 
 }
